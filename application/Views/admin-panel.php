28a03c5df453bb59c5b48ea952a17c26ffe5e917
Adam S <98590550+adamsochorec@users.noreply.github.com>
2023-12-18 17:54:21 +0100

    Admin panel front end progress

--- application/Views/admin-panel.php
+++ application/Views/admin-panel.php
@@ -2,51 +2,110 @@
 <html lang="en">
   <head>
     <?php include 'Components/head.php'; ?>
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
     <title>Product details | Adam Sochorec</title>
+    <meta name="robots" content="noindex" />
+    <meta name="googlebot" content="noindex" />
   </head>
 
-  <body>
-    <?php include 'Components/nav-bar.php'; ?>
+  <body class="store">
+    <main>
+      <?php include 'Components/nav-bar.php'; ?>
 
-    <br><br><br>
-    <div class="panel-container">
-        <div class="tab">
-            <button class="tablinks" onclick="openCity(event, 'orders')">Orders overview</button>
-            <button class="tablinks" onclick="openCity(event, 'about')">About section</button>
-            <button class="tablinks" onclick="openCity(event, 'products')">Products</button>
-            <button class="tablinks" onclick="openCity(event, 'daily')">Daily special offer</button>
-            <button class="tablinks" onclick="openCity(event, 'news')">News</button>
-        </div>
+      <article class="wrapper-standard">
+        <div class="pathname-container"></div>
+        <div class="panel-container">
+          <div class="tab">
+            <button class="tablinks" onclick="openCity(event, 'orders')">
+              Orders overview
+            </button>
+            <button class="tablinks" onclick="openCity(event, 'about')">
+              About section
+            </button>
+            <button class="tablinks" onclick="openCity(event, 'products')">
+              Products
+            </button>
+            <button class="tablinks" onclick="openCity(event, 'daily')">
+              Daily special offer
+            </button>
+            <button class="tablinks" onclick="openCity(event, 'news')">
+              News
+            </button>
+          </div>
 
-        <div id="orders" class="tabcontent">
+          <div id="orders" class="tabcontent">
             <h3>Overview of orders</h3>
-            <p>Here we will display an overview of all made orders.</p><br><br>
-            <p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p><p>London is the capital city of England.</p>
-        </div>
+            <p>Here we will display an overview of all made orders.</p>
+            <br /><br />
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+            <p>London is the capital city of England.</p>
+          </div>
 
-        <div id="about" class="tabcontent">
+          <div id="about" class="tabcontent">
             <h3>About</h3>
             <p>READ AND UPDATE</p>
             <p>About shop. About owner. Contact info. Opening/closing hours.</p>
-        </div>
+          </div>
 
-        <div id="products" class="tabcontent">
+          <div id="products" class="tabcontent">
             <h3>Products</h3>
             <p>Full CRUD for photos, print types, sizes and frames.</p>
-        </div>
+          </div>
 
-        <div id="daily" class="tabcontent">
+          <div id="daily" class="tabcontent">
             <h3>Daily</h3>
-            <p>Full CRUD for daily special offer. Select product in question. State discount percentage. Daily offer message maybe?</p>
-        </div>
+            <p>
+              Full CRUD for daily special offer. Select product in question.
+              State discount percentage. Daily offer message maybe?
+            </p>
+          </div>
 
-        <div id="news" class="tabcontent">
+          <div id="news" class="tabcontent">
             <h3>News</h3>
-            <p>CRUD for news. News are displayed on the homepage. They have a date and text contect.</p>
+            <p>
+              CRUD for news. News are displayed on the homepage. They have a
+              date and text contect.
+            </p>
+          </div>
         </div>
-    </div>
-
-    <?php include 'Components/footer.php'; ?>
+      </article>
+    </main>
+    <!-- FOOTER START -->
+    <?php include 'footer.php'; ?>
+    <!-- FOOTER END -->
   </body>
+</html>
